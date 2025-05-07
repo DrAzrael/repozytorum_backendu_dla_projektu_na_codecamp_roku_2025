c0830ac8fbfe312e10212af2f59792f07b925bd7
Wojciech Sowa <w.sowa6@gmail.com>
2025-05-07 16:08:25 +0200

    new

--- src/ollama/tools/index.ts
+++ src/ollama/tools/index.ts
@@ -1,10 +1,7 @@
 // Import tool implementations
-import findCity from "./findCity";
-import getWeather from "./getWeather";
-import getSecret from "./getSecret";
-import getIpInfo from "./getIpInfo";
+
 
 // Export all tools as a single array for toolsLoader compatibility
-const ollamaTools = [...findCity, ...getWeather, ...getSecret, ...getIpInfo];
+const ollamaTools = [];
 
 export default ollamaTools;
